3bbee720465729d421ae3aa6824d01cc51678815
Asif Shikder <33736713+Asifshikder@users.noreply.github.com>
2022-03-07 14:02:20 +0600

    dfs

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>DEFT UI is under development.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to DEFT ui" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          
         </p>
       </main>
 
